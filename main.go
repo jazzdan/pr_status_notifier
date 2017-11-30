286dab1cd07c4d06efc519733eafc2034ea7bd70
Dan Miller <danmiller@upserve.com>
2017-11-30 11:54:16 -0500

    Make it a bit more friendly

--- main.go
+++ main.go
@@ -7,23 +7,40 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/0xAX/notificator"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
 
+var notify *notificator.Notificator
+
 func main() {
+	if len(os.Args) < 4 {
+		fmt.Printf("Usage: %v <owner> <repo> <issueNumber>\n", os.Args[0])
+		os.Exit(0)
+	}
+
+	accessToken, exists := os.LookupEnv("GITHUB_ACCESS_TOKEN")
+
+	if exists != true {
+		fmt.Println("Warning: no GitHub access token detected, will only be able to check public repos")
+	}
+
+	owner := os.Args[1]
+	repo := os.Args[2]
+	prNumber, err := strconv.Atoi(os.Args[3])
+
+	notify = notificator.New(notificator.Options{
+		DefaultIcon: "icon/default.png",
+		AppName:     "Can I push?",
+	})
+
 	ctx := context.Background()
-	accessToken := os.Getenv("GITHUB_ACCESS_TOKEN")
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: accessToken},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
-
-	owner := os.Args[1]
-	repo := os.Args[2]
-	prNumber, err := strconv.ParseInt(os.Args[3], 10, 64)
-
 	if err != nil {
 		panic(err)
 	}
@@ -33,7 +50,7 @@ func main() {
 	for {
 		select {
 		case <-tick:
-			pr, _, err := client.PullRequests.Get(ctx, owner, repo, int(prNumber))
+			pr, _, err := client.PullRequests.Get(ctx, owner, repo, prNumber)
 
 			if err != nil {
 				fmt.Println(fmt.Errorf("%v", err))
@@ -41,8 +58,9 @@ func main() {
 			}
 
 			if *pr.Mergeable == true {
-				fmt.Println("Mergeable!")
-				return // I expect it to break out of the for loop here
+				message := fmt.Sprintf("Your PR %s/%s#%d is ready to be merged!", owner, repo, prNumber)
+				notify.Push("You can push!", message, "icon/default.png", notificator.UR_NORMAL)
+				return
 			}
 		}
 	}
